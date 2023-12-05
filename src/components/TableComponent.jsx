5f6b4b1374e9900d031ac65d6adbff29af545839
jamesdhw <jameshaworthwheatman@gmail.com>
2023-12-05 15:14:15 +0000

    step 3: introduce a bug by passing the ref around to a new hook

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -3,20 +3,30 @@ import ReactTable from "react-table";
 import { columns, data } from "./constants.jsx";
 
 const CustomComponent = () => {
-  const tableRef = undefined;
+  let tableRef = undefined;
 
-  const onClick = () => {
-    console.log(tableRef);
-  };
+  const { onClick } = useTable(tableRef);
 
   return (
     <>
       <button type="button" onClick={onClick}>
         Show react table ref
       </button>
-      <ReactTable data={data} columns={columns} ref={tableRef} />
+      <ReactTable
+        data={data}
+        columns={columns}
+        ref={(input) => (tableRef = input)}
+      />
     </>
   );
+};
+
+const useTable = (NOT_REALLY_THE_SAME_REF_AS_IN_THE_CALLING_COMPONENT) => {
+  const onClick = () => {
+    console.log(NOT_REALLY_THE_SAME_REF_AS_IN_THE_CALLING_COMPONENT);
+  };
+
+  return { onClick };
 };
 
 export default CustomComponent;
